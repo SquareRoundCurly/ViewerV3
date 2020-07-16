03e133aefc4b1252e5012451215e71ca70a003dd
Szoke Balazs <bala.szoke@gmail.com>
2020-07-16 16:14:01 +0200

    Test images & GIT : test images added & gitignore updated

--- Source/ViewerV3/server.js
+++ Source/ViewerV3/server.js
@@ -1,8 +1,17 @@
 'use strict';
+
+// Built-in Modules
 var http = require('http');
+var fs = require('fs');
+
+// Port declaration
 var port = process.env.PORT || 1337;
 
+// Event connect to server
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    fs.readFile('HTML/Index.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
 }).listen(port);

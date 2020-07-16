736060812559f737fdefed62345c8a93b94ba442
Szoke Balazs <bala.szoke@gmail.com>
2020-07-16 20:10:56 +0200

    ViewerV3 : static resources working

--- Source/ViewerV3/server.js
+++ Source/ViewerV3/server.js
@@ -1,20 +1,66 @@
-'use strict';
 
-// Built-in Modules
-var http = require('http');
+// PARAMETERS
+const port = 1337;
+
+// Built-in modules
+var path = require('path');
+var express = require('express');
 var fs = require('fs');
 
+// Init ExpressJS
+var app = express();
 
-// Port declaration
-var port = process.env.PORT || 1337;
+// Get static resource dir 'public'
+var dir = path.join(__dirname, 'public');
 
-// Event connect to server
-var server = http.createServer(function (req, res) {
-    fs.readFile('HTML/index.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
+// Create mime dictionary for basic resource types
+var mime =
+{
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    js: 'application/javascript'
+};
+
+// Event GET
+app.get('*', function (req, res)
+{
+    // Get file from request
+    var file = path.join(dir, req.path.replace(/\/$/, '/index.html'));
+    console.log(file);
+    // Check if file path is correct
+    if (file.indexOf(dir + path.sep) !== 0)
+    {
+        return res.status(403).end('Forbidden');
+    }
+
+    // Get file type
+    var type = mime[path.extname(file).slice(1)] || 'text/plain';
+    console.log(type);
+
+    // Stream file
+    var stream = fs.createReadStream(file);
+
+    // If stream opened successfully, pipe it back
+    stream.on('open', function ()
+    {
+        res.set('Content-Type', type);
+        stream.pipe(res);
+    });
+
+    // If stream didn't open successfully return 404
+    stream.on('error', function ()
+    {
+        res.set('Content-Type', 'text/plain');
+        res.status(404).end('Not found');
     });
 });
 
-server.listen(port);
+app.listen(port, function ()
+{
+    console.log('Listening on http://localhost:' + port + '/');
+});

da56a19ed345f4bd873c125a57c00c5efdc705ce
logik <logik@WORKSHOP>
2020-09-02 14:03:02 +0200

    ViewerV3 : dynamic sidebar radios

--- Source/ViewerV3/server.js
+++ Source/ViewerV3/server.js
@@ -41,6 +41,29 @@ let db = new sqlite3.Database('./database/test.db', sqlite3.OPEN_READWRITE | sql
     console.log('Connected to SQlite database.');
 });
 
+// Create project table
+db.run
+(`CREATE TABLE IF NOT EXISTS projects
+    (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE,
+        year INT NOT NULL,
+        projectID INT NOT NULL UNIQUE,
+        measurementTypes TEXT
+    );
+`);
+
+// Mock project data
+var projectCommand1 = "INSERT INTO projects (name,year,projectID, measurementTypes) VALUES ('0007_mintaprojekt', '2019', '7', 'x-sec');";
+var projectCommand2 = "INSERT INTO projects (name,year,projectID, measurementTypes) VALUES ('0012_mintaprojekt', '2019', '12', 'x-sec xray');";
+var projectCommand3 = "INSERT INTO projects (name,year,projectID, measurementTypes) VALUES ('0018_mintaprojekt', '2019', '18', 'x-sec');";
+var projectCommand4 = "INSERT INTO projects (name,year,projectID, measurementTypes) VALUES ('0028_mintaprojekt', '2019', '28', 'x-sec xray shear');";
+
+db.run(projectCommand1, function (err) { if (err !== null) console.log(err); });
+db.run(projectCommand2, function (err) { if (err !== null) console.log(err); });
+db.run(projectCommand3, function (err) { if (err !== null) console.log(err); });
+db.run(projectCommand4, function (err) { if (err !== null) console.log(err); });
+
 // Create image table
 db.run
 (`CREATE TABLE IF NOT EXISTS images
@@ -139,30 +162,39 @@ app.get('*', function (req, res)
 // Connection event (and disconnect)
 io.on('connection', (socket) =>
 {
+    // Connection
     console.log('a user connected');
 
+    // TODO: remove this
     fs.readFile('../../Images/S115_C413_02.jpg', function (err, data)
     {
         console.log('emitting S115_C413_02.jpg');
         socket.emit('image', "data:image/jpg;base64," + data.toString("base64"));
     });
 
+    // Search event
     socket.on('SearchBtn', function (data) {
         Search(data.serial, data.type);
         console.log("emitting array of size : " + result.length);
     });
 
+    // Search result emission
     if (result.length > 0) {
         socket.emit('searchResult', { result: result[0] });
         resultIterator += 1;
     }
 
+    // Ready for next search result emission
     socket.on('searchDigested', function () {
         if (resultIterator < result.length) {
             socket.emit('searchResult', { result: result[resultIterator] });
             resultIterator += 1;
         }
     });
+
+    socket.on('ProjectSearchBtn', function (data) {
+        SearchProject(data.year, data.project);
+    });    
 
     socket.on('disconnect', () =>
     {
@@ -205,6 +237,47 @@ function Search(serial, type) {
 
             
         });
+    });
+}
+
+function SearchProject(year, projectID) {
+    console.log('Searching for project : ' + projectID + ' in year : ' + year);
+
+    // Construct command
+    console.log("Constructed query");
+    var command = "SELECT name, year, projectID, measurementTypes FROM projects WHERE year = " + year + " AND projectID = " + projectID + ";";
+    console.log(command);
+
+    // Get results from SELECT
+    var projectResult = { name: "", year: -1, projectID: -1, measurementTypes: "" };
+    db.all(command, [], (err, rows) => {
+
+        if (err !== null) {
+            console.log(err);
+        }
+
+        var success = false;
+        console.log("Found " + rows.length + " results");
+        if (rows.length == 1) {
+            console.log("Found matching project");
+            success = true;
+        }
+        else if (rows.length == 0) {
+            console.log("No such project matching year and projectID");
+        }
+        else {
+            console.log("Incorrect project query, multiple results matching parameters");
+        }
+
+        if (success) {
+            console.log("Found project name : " + rows[0].name);
+            projectResult.name = rows[0].name;
+            projectResult.year = rows[0].year;
+            projectResult.projectID = rows[0].projectID;
+            projectResult.measurementTypes = rows[0].measurementTypes;
+        }
+
+        io.sockets.emit("projectSearchResult", projectResult);
     });
 }
 

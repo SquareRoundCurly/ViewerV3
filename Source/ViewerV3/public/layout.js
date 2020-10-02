47fbf8449d7136935b5e7932d062ccb14a855cb8
logik <logik@WORKSHOP>
2020-10-02 15:26:26 +0200

    ViewerV3 : sidepanel layout replication

--- Source/ViewerV3/public/layout.js
+++ Source/ViewerV3/public/layout.js
@@ -14,4 +14,30 @@ window.addEventListener('load', function () {
         $grid.packery('bindDraggabillyEvents', draggie);
         //gridItem.innerHTML = '<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg" />';
     });
+
+    function onLayout() {
+        console.log('fitComplete'/*'layout done'*/);
+
+        // Set layout grid
+        var html = document.getElementById('grid').innerHTML;
+        document.getElementById('grid2').innerHTML = html;
+
+        var grid = document.querySelector('.grid2')
+        var pckry = Packery.data(grid);
+
+        pckry.reloadItems();
+
+        var elems = pckry.getItemElements()
+        //elems.forEach((element) => {
+        //    var draggie = new Draggabilly(element);
+        //    // bind drag events to Packery
+        //    pckry.bindDraggabillyEvents(draggie);
+        //});
+
+        //pckry.shiftLayout();
+        pckry.reloadItems();
+    }
+
+    // bind event listener
+    $grid.on('dragItemPositioned', onLayout);
 });
